475d8d169abdc67dd9a35e90e1432d12e08c2ae3
Glenn444 <gmark1586@gmail.com>
2024-10-08 08:41:00 +0300

    0. Basic list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,23 @@
+const students = [
+  {
+    id: 1,
+    fistName: 'Guillaume',
+    location: 'San Francisco',
+  },
+  {
+    id: 2,
+    fistName: 'James',
+    location: 'Columbia',
+  },
+  {
+    id: 5,
+    fistName: 'Serena',
+    location: 'San Francisco',
+  },
+];
+
+function getListStudents() {
+  return students;
+}
+
+module.exports = getListStudents;
